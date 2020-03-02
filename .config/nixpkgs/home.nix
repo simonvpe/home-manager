7409da32790c7da4a6e625fa746c08f1311208ab
Simon Pettersson <simon.v.pettersson@gmail.com>
2020-03-02 22:30:50 +0100

    Change to neovim

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -16,7 +16,6 @@ in {
      powerline-fonts    # we like fonts
      ripgrep            # like grep, but better
      strace             # trace what applications do
-     vim                # edit the shit out of files
   ];
 
   xsession = {
@@ -84,5 +83,58 @@ in {
     enable = true;
   };
 
-  home.file.".vimrc".source = ./.vimrc;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    withPython3 = true;
+    plugins = with pkgs.vimPlugins; [
+      ale                    # Asynchronous Lint Engine
+      fzf-vim                # A command-line fuzzy finder
+      indentLine             # A vim plugin to display the indention levels with thin vertical lines
+      rainbow_parentheses    # Simpler Rainbow Parentheses
+      semshi                 # Semantic Highlighting for Python in Neovim
+      vim-airline            # lean & mean status/tabline for vim that's light as air
+      vim-better-whitespace  # Better whitespace highlighting for Vim
+      vim-commentary         # Comment stuff out
+      vim-eunuch             # Helpers for UNIX
+      vim-fugitive           # A Git wrapper so awesome, it should be illegal
+      vim-hoogle             # Vim plugin used to query hoogle
+      vim-nix                # Vim configuration files for Nix
+      vim-sensible           # Defaults everyone can agree on
+      vim-signify            # Show a diff using Vim its sign column
+      vim-surround           # Quoting/parenthesizing made simple
+    ];
+    extraConfig = ''
+      " General settings
+      syntax on
+      set number relativenumber
+      set ff=unix
+      set tabstop=4
+      set shiftwidth=4
+      set expandtab
+      set cursorline
+      set list
+      set listchars=tab:>-
+      set noswapfile
+      set nobackup
+
+      " Ripgrep and Fzf bindings
+      nnoremap <C-b> :Buffers<Cr>
+      nnoremap <C-t> :Files<Cr>
+      nnoremap <C-h> :Rg<Cr>
+      nnoremap <C-l> :BLines<Cr>
+      nnoremap <C-L> :Lines<Cr>
+
+      " Setup rainbow_parentheses
+      au VimEnter * RainbowParenthesesToggle
+      au Syntax * RainbowParenthesesLoadRound
+      au Syntax * RainbowParenthesesLoadSquare
+      au Syntax * RainbowParenthesesLoadBraces
+
+      " Whitespace settings
+      let g:strip_whitespace_on_save = 1
+      let g:strip_whitespace_confirm = 0
+    '';
+  };
 }
